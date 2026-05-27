52d1b755560dc59bf0d35c5832997f8fcdbdaf9b
Bhavna Mehetre <bhavnamehetre3@gmail.com>
2026-05-27 22:57:27 +0530

    Add files via upload

--- a.php
+++ a.php
@@ -1,3 +1,45 @@
-1,bhavna,101,georai
-2,shraddha,102,pune
-3,sham,103,nashik
+<html>
+    <body>
+        <form method="POST" action="#">
+            enter file:<input type="text" name="f1"><br>
+            type of file:<input type="radio" name="f2" value="1"><br>
+            last access time:<input type="radio" name="f2" value="2"><br>
+            size of file:<input type="radio" name="f2" value="3"><br>
+            delete the  file:<input type="radio" name="f2" value="4"><br>
+            <input type="submit" value="submit">
+</form>
+</body>
+<html>
+    <?php
+
+    $a=$_POST["f1"];
+    $ch=$_POST["f2"];
+    switch($ch)
+    {
+        case 1:
+            $b=filetype($a);
+            echo $b;
+            break;
+
+            case 2:$b=fileatime($a);
+            echo $b;
+            break;
+
+            case 3:$b=filesize($a);
+            echo $b;
+            break;
+
+            case 4:
+                if(file_exists($a))
+                {
+                    unlink($a);
+                    echo "deleted...!";
+                }
+                else
+                {
+                    echo "not...!";
+                }
+                break;
+
+
+    }
