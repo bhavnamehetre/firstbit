68f45d7869c3116efcb975d6302db0d924c2aece
Bhavna Mehetre <bhavnamehetre3@gmail.com>
2026-06-01 11:07:26 +0530

    Add files via upload

---
+++ 16.php
@@ -0,0 +1,21 @@
+<html>
+    <body>
+        <script type="text/javascript">
+            var c=confirm("Exam is near,have you praparing for");
+            if(c==true)
+            {
+                var p=window.prompt("enter your name");
+                if(p!=null)
+                {
+                    window.alert("best of luck....!"+p);
+                }
+                
+            }
+            else
+                {
+                    window.alert("plaese do study");
+                }
+            </script>
+            </body>
+</html>
+        
