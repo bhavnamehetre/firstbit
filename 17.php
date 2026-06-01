9ea732e83e4c4b66b758f999915f51a11be7d310
Bhavna Mehetre <bhavnamehetre3@gmail.com>
2026-06-01 11:07:46 +0530

    Add files via upload

---
+++ 17.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+         <script src="jquery-4.0.0.js"></script>
+</head>
+    <body>
+        <script>
+       
+            $(document).ready(function()
+            {
+            
+            $("p").append("<h2>hello bhavna</h2>");
+            $("ol").append("shraddha diiiiiii");
+           }
+        
+        
+        
+        );
+        </script>
+        <ol type="1">
+            <li>bhavna</li>
+            <li>pinkii</li>
+            <li>rutu</li>
+            <li>sharddha</li>
+</ol>
+<p>bhavna somnath mehetre i am from georai</p>
+</body>
+</html>
