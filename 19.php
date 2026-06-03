36521ee6d064cb8db1695d316b807b1d30310175
Bhavna Mehetre <bhavnamehetre3@gmail.com>
2026-06-03 18:55:15 +0530

    Add files via upload

---
+++ 19.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <script src="jquery-4.0.0.js"></script>
+</head>
+<body>
+    <script>
+        $(document).ready(function()
+    {
+       $("img").after("<h1>hello</h1>");
+       $("img").before("<h1>hello</h1>");
+
+    });
+    </script>
+    <img src="AAJ_1943.JPG" height="800px"  width="800px">
+</body>
+</html>
