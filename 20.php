a486f448b03f2dfaed91c008b2159b315b328ac4
Bhavna Mehetre <bhavnamehetre3@gmail.com>
2026-06-03 19:00:48 +0530

    Add files via upload

---
+++ 20.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <script>
+            function pageload()
+            {
+                alert("page loaded...");
+            }
+            function validname()
+            {
+                var name=document.getElementById("sname").value;
+                if(name!=" ")
+                {
+                    document.getElementById("sname")
+                }
+            }
